b023edf7d6377bc32346c92c5b1bf78cfb9e2ec8
Joao Victor <joaoaraujo.367@gmail.com>
2020-01-18 01:50:24 -0300

    Update execute activity

--- lib/pages/execute_activity.dart
+++ lib/pages/execute_activity.dart
@@ -10,7 +10,6 @@ import 'package:authorship/models/activity.dart';
 import 'package:authorship/models/location.dart';
 import 'package:authorship/models/content.dart';
 
-
 class ExecuteActivity extends StatefulWidget {
   final String url = "https://class-path-location.herokuapp.com/distance/";
   final Activity activity;
@@ -18,9 +17,9 @@ class ExecuteActivity extends StatefulWidget {
   final Content content;
 
   ExecuteActivity({Key key, @required this.activity})
-    : this.location = activity.location,
-      this.content = activity.content,
-      super(key: key);
+      : this.location = activity.location,
+        this.content = activity.content,
+        super(key: key);
 
   @override
   State<ExecuteActivity> createState() {
@@ -33,36 +32,23 @@ class ExecuteActivityState extends State<ExecuteActivity> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   Future<String> _distance(Position position) async {
-    List origin = [
-      position.latitude, position.longitude
-    ];
-    List destination = [
-      widget.location.latitude, widget.location.longitude
-    ];
+    List origin = [position.latitude, position.longitude];
+    List destination = [widget.location.latitude, widget.location.longitude];
 
-    Map data = {
-      'origin': origin,
-      'destination': destination,
-      'threshold': 3
-    };
+    Map data = {'origin': origin, 'destination': destination, 'threshold': 10};
 
-    var response = await http.post(
-      Uri.encodeFull(widget.url),
-      body: json.encode(data),
-      headers: {
-        'Content-type': 'application/json'
-      }
-    );
+    var response = await http.post(Uri.encodeFull(widget.url),
+        body: json.encode(data), headers: {'Content-type': 'application/json'});
 
     String body = utf8.decode(response.bodyBytes);
 
     if (response.statusCode == 200) {
       Map dataJson = json.decode(body);
-      
+
       if (dataJson['threshold']) {
         contentController.text = widget.content.description;
       } else {
-        contentController.text = dataJson['threshold'].toString();
+        contentController.text = "Localização incorreta";
       }
 
       scaffoldKey.currentState
@@ -81,41 +67,56 @@ class ExecuteActivityState extends State<ExecuteActivity> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test Activity"),
+        title: Text("Teste da atividade"),
       ),
       key: scaffoldKey,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          TextFormField(
-            enabled: false,
-            maxLines: 5,
-            decoration: InputDecoration(
-              labelText: "Atividade",
-              border: OutlineInputBorder(),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              enabled: false,
+              maxLines: 5,
+              decoration: InputDecoration(
+                labelText: "Atividade",
+                border: OutlineInputBorder(),
+              ),
+              initialValue: widget.activity.description,
             ),
-            initialValue: widget.activity.description,
           ),
           Container(
+            margin: const EdgeInsets.all(15.0),
             child: Center(
-              child: RaisedButton(
-                child: Text("Send location"),
-                onPressed: () async {
-                  Position position = await Geolocator().getCurrentPosition(
-                    desiredAccuracy: LocationAccuracy.high
-                  );
+              child: ButtonTheme(
+                minWidth: 200.0,
+                height: 50.0,
+                child: RaisedButton(
+                  child: Text("Verificar localização"),
+                  onPressed: () async {
+                    Position position = await Geolocator().getCurrentPosition(
+                        desiredAccuracy: LocationAccuracy.high);
 
-                  _distance(position);
-                },
+                    _distance(position);
+                  },
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                ),
               ),
             ),
           ),
-          TextFormField(
-            enabled: false,
-            maxLines: 5,
-            controller: contentController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: TextFormField(
+              enabled: false,
+              maxLines: 5,
+              controller: contentController,
+              decoration: InputDecoration(
+                labelText: "Conteúdo",
+                border: OutlineInputBorder(),
+              ),
             ),
           ),
         ],
